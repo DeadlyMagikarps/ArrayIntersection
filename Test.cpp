dc34377bd19ad5eb4893bf01ba2010a9afd901ef
DeadlyMagikarps <hrumjahn@nevada.unr.edu>
2020-04-12 18:03:37 -0700

    Redid program to find intersection using linked list. Added Node.h file

--- Test.cpp
+++ Test.cpp
@@ -5,9 +5,23 @@
 // Output 10, 54
 
 #include <iostream>
+#include "Node.h"
 using namespace std;
 
-// Assuming that these are sorted arrays
+// Insert a new node to the list
+void insert(Node** root, int item);
+
+// Create a linked list with given array list
+Node* arrayToList(int arr[], int size);
+
+// Function to get the size of the linked list (# of nodes)
+int getLinkedListCount(Node* head);
+
+// Function to get the intersection when one list has more nodes than the other list
+void getIntersection(Node* head1, Node* head2);
+
+// Function to get the intersection of equal length lists
+void getIntersectionHelper(int numNodes, Node* head1, Node* head2);
 
 // Function to print intersection
 /*
@@ -25,12 +39,161 @@ int main()
     int arrSize1 = sizeof(listArray)/sizeof(listArray[0]);
     int arrSize2 = sizeof(listArray2)/sizeof(listArray2[0]);
 
+    Node* list1 = arrayToList(listArray, arrSize1);
+    Node* list2 = arrayToList(listArray2, arrSize2);
+
+    // Get the intersection of the two lists
+    getIntersection(list1, list2);
+
     // Print the intersections found between the arrays
-    printIntersection(listArray, listArray2, arrSize1, arrSize2);
+    //printIntersection(listArray, listArray2, arrSize1, arrSize2);
 
     return 0;
 }
 
+// Function Implementations
+void insert(Node** head, int item)
+{
+    Node* temp = new Node;
+    Node* ptr;
+
+    // Set the item to the new node temp data
+    // set the next item to null
+    temp->data = item;
+    temp->next = NULL;
+
+    if(*head == NULL)
+    {
+        *head = temp;
+    }
+
+    else
+    {
+        ptr = *head;
+        
+        while(ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+
+        ptr->next = temp;
+    }
+    
+}
+
+Node* arrayToList(int arr[], int size)
+{
+    int index;
+    Node* head = NULL;
+
+    for(index = 0; index < size; index++)
+    {
+        insert(&head, arr[index]);
+    }
+
+    return head;
+}
+
+int getLinkedListCount(Node* head)
+{
+    // Initialize variables
+    int count = 0;
+
+    // Set the current pointer to the head of the list
+    Node* current = head;
+
+
+    // start the count until it hits NULL. Break after and return count
+    while(current != NULL)
+    {
+        // increment counter
+        count++;
+        
+        // move the pointers
+        current = current->next;
+    }
+
+    return count;
+}
+
+void getIntersection(Node* head1, Node* head2)
+{
+    // Initialize variables
+    int numNodes;
+
+    // get the sizes of both linked lists
+    int llsize_1 = getLinkedListCount(head1);
+    int llsize_2 = getLinkedListCount(head2);
+
+    // check if the first list has more nodes than the second list
+    if(llsize_1 > llsize_2)
+    {
+        numNodes = llsize_1 - llsize_2;
+        getIntersectionHelper(numNodes, head1, head2);
+    }
+
+    // otherwise, the opposite
+    else
+    {
+        numNodes = llsize_2 - llsize_1;
+        getIntersectionHelper(numNodes, head2, head1);
+    }
+}
+
+// This functions starts out with the bigger list to compare and increment
+void getIntersectionHelper(int numNodes, Node* head1, Node* head2)
+{
+    // Initialize variables
+    int index;
+    Node* current1 = head1;
+    Node* current2 = head2;
+
+    // move the pointer down the list
+    for(index = 0; index < numNodes; index++)
+    {
+        // check for null and do nothing
+        if(current1 == NULL)
+        {
+            return;
+        }
+
+        // Move the pointer
+        else
+        {
+            current1 = current1->next;
+        }
+    }
+
+    // Move the pointers of both lists till a match is found
+    while(current1 != NULL && current2 != NULL)
+    {
+        // Compare the data itself to each other to see if they match
+        // Current data in list 1 is less than the current data in list 2
+        if(current1->data < current2->data)
+        {
+            // return the data
+            current1 = current1->next;
+        }
+        
+        // Current data in list 2 is less than the current data in list 1
+        else if(current2->data < current1->data)
+        {
+            current2 = current2->next;
+        }
+
+        // Current Data in both lists match!
+        else
+        {
+            cout << current2->data << " ";
+                    
+            // move both pointers
+            current1 = current1->next;
+            current2 = current2->next;
+        }
+    }
+}
+
+// Array based function
 void printIntersection(int arr[], int arr2[], int arrSize, int arrSize2)
 {
     // Initialize variables
